c47543debfebd33921248a5bd659d00827526fd5
prasad373 <prasadsachinist10@gmail.com>
2022-02-22 20:38:06 +0530

    Signin button working properly

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useState } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-import { Route, Routes } from "react-router-dom";
+import { Link, NavLink, Route, Routes } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 
@@ -18,27 +18,20 @@ const App = () => {
 
   return (
     <>
-    <div>
+      <div>
         <Navbar />
         <LoadingBar
           color='#f11946'
           height={3}
           progress={progress}
         />
-        
+
         <Routes>
-          <Route path="/"><News setProgress={setProgress} apiKey={apiKey}element={<genral />} pageSize={pageSize} country="in" category="general" /></Route>
-          <Route path="/business"><News setProgress={setProgress} apiKey={apiKey}element={<business />} pageSize={pageSize} country="in" category="business" /></Route>
-          <Route path="/entertainment"><News setProgress={setProgress} apiKey={apiKey}element={<entertainment />} pageSize={pageSize} country="in" category="entertainment" /></Route>
-          <Route path="/health"><News setProgress={setProgress} apiKey={apiKey}element={<health/>} pageSize={pageSize} country="in" category="health" /></Route>
-          <Route path="/science"><News setProgress={setProgress} apiKey={apiKey}element={<science />} pageSize={pageSize} country="in" category="science" /></Route>
-          <Route path="/sports"><News setProgress={setProgress} apiKey={apiKey}element={<sports />} pageSize={pageSize} country="in" category="sports" /></Route>
-          <Route path="/technology"><News setProgress={setProgress} apiKey={apiKey}element={<technology />} pageSize={pageSize} country="in" category="technology" /></Route>
-          <Route path="/general"><News setProgress={setProgress} apiKey={apiKey}element={<genral />} pageSize={pageSize} country="in" category="general" /></Route>
-          {/*<Route path="/Home"><News setProgress={setProgress} apiKey={apiKey}element={<Home />} pageSize={pageSize} country="in" category="general" /></Route>*/}
-          {/*<Route path="/News24"><News setProgress={setProgress} apiKey={apiKey}element={<News24 />} pageSize={pageSize} country="in" category="general" /></Route>*/}
-          {/*<Route path="/Signin" element={<signin/>}></Route>*/}
-      </Routes>
+          <NavLink to='/Components/News'><News setProgress={setProgress} apiKey={apiKey} pageSize={pageSize} country="in" category="general" /></NavLink>
+          <NavLink to='/Copmonents/business'><News setProgress={setProgress} apiKey={apiKey} pageSize={pageSize} country="in" category="business" /></NavLink>
+          <Link to='/Components/Signin'></Link>
+          <Link to='/Components/Login'></Link>
+        </Routes>
       </div>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from './components/Login';
+import Login from './Components/Login';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './Components/Signin';
+import News from './Components/News';
 
 ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={ <App /> } />
         <Route exact path='/Login' element={ <Login /> } />
+        <Route exact path='/Signin' element={ <Signin /> } />
+        <Route exact path='/News24' element={ <News /> } />
+        <Route exact path='/business' element={ <business /> } />
       </Routes>
     </BrowserRouter>,
   document.getElementById('root')
