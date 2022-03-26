1f4c47556843c6103b0e7eb65b06421ff9388264
DSAghicha <dsaghicha211@gmail.com>
2022-03-27 00:44:51 +0530

    Delete backend redundant folder

--- client/src/components/Backend/Mongodb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI ="mongodb+srv://Prasad_pathare:prasad103@cluster0.8w9bl.mongodb.net/NEWSAPP.News24?retryWrites=true&w=majority"
-
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, () => {
-        console.log("Connected to Mongo Successfully");
-    })
-}
-module.exports = connectToMongo;
